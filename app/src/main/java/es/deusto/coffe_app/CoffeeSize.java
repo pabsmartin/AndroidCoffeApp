1080826163ed2885086b279af8214a3dd6a981d4
pabsmartin <pablomartin@opendeusto.es>
2020-06-14 16:16:04 +0200

    mood and size activities and styles

--- app/src/main/java/es/deusto/coffe_app/CoffeeSize.java
+++ app/src/main/java/es/deusto/coffe_app/CoffeeSize.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LandingActivity extends AppCompatActivity {
+public class CoffeeSize extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_coffee_size);
     }
 }
