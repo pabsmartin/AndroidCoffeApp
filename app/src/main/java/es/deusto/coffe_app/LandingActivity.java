8f4d8395aa2175cd919cbdce3a086632641ca9c4
pabsmartin <pablomartin@opendeusto.es>
2020-06-14 12:36:45 +0200

    style and color

--- app/src/main/java/es/deusto/coffe_app/LandingActivity.java
+++ app/src/main/java/es/deusto/coffe_app/LandingActivity.java
@@ -18,18 +18,6 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        float val = 1.3f;
-        fab.setScaleY(val);
-        fab.setScaleX(val);
     }
 
     @Override
